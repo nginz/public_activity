d8aa21389674778641a1b5bf4657bfb2fe11e1c4
Moustafa Badawy <moustafa@rubikal.com>
2013-04-22 04:44:23 +0200

    Association extensions for read & unread

--- lib/public_activity/orm/active_record/trackable.rb
+++ lib/public_activity/orm/active_record/trackable.rb
@@ -3,7 +3,15 @@ module PublicActivity
     module ActiveRecord
       module Trackable
         def self.included(base)
-          base.has_many :activities, :class_name => "::PublicActivity::Activity", :as => :trackable
+          base.has_many :activities, :class_name => "::PublicActivity::Activity", :as => :trackable do
+            def read
+              where is_read: true
+            end
+
+            def unread
+              where is_read: false
+            end
+          end
         end
       end
     end
