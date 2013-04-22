bd596cbce2f4d928b4483cd7f660254d06f6d76e
Moustafa Badawy <moustafa@rubikal.com>
2013-04-22 05:20:36 +0200

    Reverted class method changes

--- lib/public_activity/readable.rb
+++ lib/public_activity/readable.rb
@@ -1,9 +1,5 @@
 module PublicActivity
   module Readable
-    def self.included(base)
-      base.extend(ClassMethods)
-    end
-
     def is_read?
       is_read
     end
@@ -23,20 +19,6 @@ module PublicActivity
       unless self.is_unread?
         self.is_read = false
         self.save
-      end
-    end
-
-    module ClassMethods
-      def mark_read!
-        each do |activity|
-          activity.mark_read!
-        end
-      end
-
-      def mark_unread!
-        each do |activity|
-          activity.mark_unread!
-        end
       end
     end
   end
