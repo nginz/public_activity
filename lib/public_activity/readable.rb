b20377b9a26c6ecf47b4f67e3b3b4bd28cb480af
Moustafa Badawy <moustafa@rubikal.com>
2013-04-22 05:11:11 +0200

    Added class level mark_read/unread

--- lib/public_activity/readable.rb
+++ lib/public_activity/readable.rb
@@ -1,5 +1,9 @@
 module PublicActivity
   module Readable
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
     def is_read?
       is_read
     end
@@ -19,6 +23,20 @@ module PublicActivity
       unless self.is_unread?
         self.is_read = false
         self.save
+      end
+    end
+
+    module ClassMethods
+      def mark_read!
+        each do |activity|
+          activity.mark_read!
+        end
+      end
+
+      def mark_unread!
+        each do |activity|
+          activity.mark_unread!
+        end
       end
     end
   end
